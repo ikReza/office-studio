7eb563b5a1d1e8bc19680db29702949ba325edb1
ikReza <ireza.kaiser00@gmail.com>
2020-07-14 19:32:51 +0600

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import NavigationBar from "./components/NavigationBar";
+
 function App() {
-  return <div className="App">Hello!</div>;
+  return (
+    <>
+      <NavigationBar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+
+const NavigationBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  let location = window.location.href;
+
+  return (
+    <Navbar fixed="top" expand="md" style={{ backgroundColor: "#233" }}>
+      <div className="container">
+        <NavbarBrand href="#page-top" className="navbrand">
+          Lorem Ipsum
+        </NavbarBrand>
+        <NavbarToggler
+          onClick={() => setIsOpen(!isOpen)}
+          className="nav-toggle"
+        >
+          <i className="fa fa-bars"></i>
+        </NavbarToggler>
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto text-uppercase" navbar>
+            <NavItem>
+              <NavLink
+                href="#services"
+                className={
+                  location === "http://localhost:3000/#services"
+                    ? "active"
+                    : "inactive"
+                }
+              >
+                Services
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                href="#portfolio"
+                className={
+                  location === "http://localhost:3000/#portfolio"
+                    ? "active"
+                    : "inactive"
+                }
+              >
+                Portfolio
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                href="#about"
+                className={
+                  location === "http://localhost:3000/#about"
+                    ? "active"
+                    : "inactive"
+                }
+              >
+                About
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                href="#team"
+                className={
+                  location === "http://localhost:3000/#team"
+                    ? "active"
+                    : "inactive"
+                }
+              >
+                Team
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                href="#contact"
+                className={
+                  location === "http://localhost:3000/#contact"
+                    ? "active"
+                    : "inactive"
+                }
+              >
+                Contact
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </div>
+    </Navbar>
+  );
+};
+
+export default NavigationBar;
