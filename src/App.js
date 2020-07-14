d0e78cee28b055c0bb0471cb4b51e698df39d9b1
ikReza <ireza.kaiser00@gmail.com>
2020-07-14 20:34:48 +0600

    service section added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./components/styles/style.css";
 
 import NavigationBar from "./components/NavigationBar";
 import WelcomePage from "./components/WelcomePage";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
 
       <section id="page-top">
         <WelcomePage />
+      </section>
+
+      <section className="page-section" id="services">
+        <Services />
       </section>
     </>
   );
