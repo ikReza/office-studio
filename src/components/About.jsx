75ee419bc16b8184efcf7b49a0e91e0c79299831
ikReza <ireza.kaiser00@gmail.com>
2020-07-20 19:14:48 +0600

    material-ui timeline added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,29 +1,42 @@
 import React from "react";
+import {
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+} from "@material-ui/lab";
+import { Typography, Box, Hidden } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-const journey = [
-  {
-    image: "images/1.jpg",
-    year: "2009-2011",
-    title: "Our Humble Beginings",
+import { journey } from "./project";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: "6px 16px",
   },
-  {
-    image: "images/2.jpg",
-    year: "March 2011",
-    title: "An Agency is Born",
+  pic: {
+    borderRadius: "50%",
+    height: "120px",
+    width: "120px",
   },
-  {
-    image: "images/3.jpg",
-    year: "December 2012",
-    title: "Transition to Full Service",
+  box: {
+    borderRadius: "50%",
+    height: "120px",
+    width: "120px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fed136",
   },
-  {
-    image: "images/4.jpg",
-    year: "July 2014",
-    title: "Phase Two Expansion",
+  timeItem: {
+    minHeight: "45vh",
   },
-];
+}));
 
 const About = () => {
+  const classes = useStyles();
   return (
     <div className="container">
       <div className="row">
@@ -34,78 +47,103 @@ const About = () => {
           </h3>
         </div>
       </div>
-      <div className="row">
-        <ul className="timeline">
-          {journey.map((go, i) => (
-            <li className="list-unstyled about-list" key={i}>
-              <div className="row row-content align-items-center">
-                <div className="col-11 col-sm-11 col-md-3">
-                  <div className="timeline-image">
-                    <img
-                      className="rounded-circle img-fluid"
-                      src={go.image}
-                      alt={go.title}
-                    />
+      <Hidden smDown>
+        <Timeline align="alternate">
+          {journey.map((j, i) => (
+            <TimelineItem key={i} className={classes.timeItem}>
+              <TimelineSeparator>
+                <TimelineDot>
+                  <img className={classes.pic} src={j.image} alt={j.title} />
+                </TimelineDot>
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Box className={classes.paper}>
+                  <Typography variant="h6" component="h1">
+                    {j.year}
+                  </Typography>
+                  <Typography variant="h6" component="h1">
+                    {j.title}
+                  </Typography>
+                  <Typography style={{ textAlign: "justify" }}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Sunt ut voluptatum eius sapiente, totam reiciendis
+                    temporibus qui quibusdam, recusandae sit vero unde, sed,
+                    incidunt et ea quo dolore laudantium consectetur!
+                  </Typography>
+                </Box>
+              </TimelineContent>
+            </TimelineItem>
+          ))}
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot>
+                <Box className={classes.box}>
+                  <Typography align="center" variant="h6">
+                    Be Part <br />
+                    of Our <br />
+                    Story
+                  </Typography>
+                </Box>
+              </TimelineDot>
+            </TimelineSeparator>
+            <TimelineContent></TimelineContent>
+          </TimelineItem>
+        </Timeline>
+      </Hidden>
+      <Hidden mdUp>
+        <div className="row">
+          <ul className="timeline">
+            {journey.map((go, i) => (
+              <li className="list-unstyled about-list" key={i}>
+                <div className="row row-content align-items-center">
+                  <div className="col-11 col-sm-11 col-md-3">
+                    <div className="timeline-image">
+                      <img
+                        className="rounded-circle img-fluid"
+                        src={go.image}
+                        alt={go.title}
+                      />
+                    </div>
+                  </div>
+                  <div className="col-11 col-sm-11 col-md">
+                    <div className="timeline-panel">
+                      <div className="timeline-heading">
+                        <h4>{go.year}</h4>
+                        <h4 className="subheading">{go.title}</h4>
+                      </div>
+                      <div className="timeline-body">
+                        <p className="text-muted text-justify">
+                          Lorem ipsum dolor sit amet, consectetur adipisicing
+                          elit. Sunt ut voluptatum eius sapiente, totam
+                          reiciendis temporibus qui quibusdam, recusandae sit
+                          vero unde, sed, incidunt et ea quo dolore laudantium
+                          consectetur!
+                        </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div className="col-11 col-sm-11 col-md">
-                  <div className="timeline-panel">
-                    <div className="timeline-heading">
-                      <h4>{go.year}</h4>
-                      <h4 className="subheading">{go.title}</h4>
-                    </div>
-                    <div className="timeline-body">
-                      <p className="text-muted text-justify">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Sunt ut voluptatum eius sapiente, totam reiciendis
-                        temporibus qui quibusdam, recusandae sit vero unde, sed,
-                        incidunt et ea quo dolore laudantium consectetur!
-                      </p>
-                    </div>
+              </li>
+            ))}
+            <li className=" list-unstyled list-bottom">
+              <div className="row row-content align-items-center">
+                <div className="col-4 col-sm-4 col-md-3">
+                  <div className="timeline-image">
+                    <h4>
+                      Be Part
+                      <br />
+                      Of Our
+                      <br />
+                      Story!
+                    </h4>
                   </div>
                 </div>
               </div>
             </li>
-          ))}
-          <li className=" list-unstyled list-bottom">
-            <div className="row row-content align-items-center">
-              <div className="col-4 col-sm-4 col-md-3">
-                <div className="timeline-image">
-                  <h4>
-                    Be Part
-                    <br />
-                    Of Our
-                    <br />
-                    Story!
-                  </h4>
-                </div>
-              </div>
-              <div className="col col-sm-4 col-md-3">
-                <div className="timeline-image">
-                  <h4>
-                    Share
-                    <br />
-                    Our
-                    <br />
-                    Dream!
-                  </h4>
-                </div>
-              </div>
-              <div className="col-4 col-sm col-md-5">
-                <div className="timeline-image">
-                  <h4>
-                    Believe
-                    <br />
-                    In
-                    <br />
-                    Yourself!
-                  </h4>
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
+          </ul>
+        </div>
+      </Hidden>
     </div>
   );
 };
---
+++ src/components/project.js
@@ -0,0 +1,22 @@
+export const journey = [
+  {
+    image: "images/1.jpg",
+    year: "2009-2011",
+    title: "Our Humble Beginings",
+  },
+  {
+    image: "images/2.jpg",
+    year: "March 2011",
+    title: "An Agency is Born",
+  },
+  {
+    image: "images/3.jpg",
+    year: "December 2012",
+    title: "Transition to Full Service",
+  },
+  {
+    image: "images/4.jpg",
+    year: "July 2014",
+    title: "Phase Two Expansion",
+  },
+];
